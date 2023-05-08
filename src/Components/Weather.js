d8035652cceffca105c09997b6d6326804d95fa9
Renautis Patterson <123973708+CodeMell@users.noreply.github.com>
2023-05-08 13:45:36 -0500

    Update Weather.js

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -1,7 +1,13 @@
-function Weather(props) {
+import axios from "axios";
+
+function Weather() {
+    getWeather = async () =>{
+        const API = 'http://localhost:3001';
+        const response = await axios.get(`${API}/weather`);
+    }
     return (
       <div>
-        
+
       </div>
     );
   }
