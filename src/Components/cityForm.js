211749a710f0e1a515aeb97e8a4dcbd48fd3ddaa
Renautis Patterson <123973708+CodeMell@users.noreply.github.com>
2023-05-19 10:22:22 -0500

    up

--- src/Components/cityForm.js
+++ src/Components/cityForm.js
@@ -4,11 +4,13 @@ import axios from 'axios';
 import React, { useState } from 'react';
 import CityMap from './cityMap';
 import Weather from './Weather';
+import Movies from './Movies';
 
 function CityForm() {
     const [searchQuery, setSearchQuery] = useState(0);
     const [location, setlocation] = useState(0);
     const [weatherData, setWeatherData] = useState([]);
+    const [moviesData, setMoviesData] = useState({results:[]});
 
     // State = {searchQuery:'',location: {}}
 
@@ -20,20 +22,37 @@ function CityForm() {
         setlocation(res.data[0]);
         let reW = await getWeatherData(res.data[0].lat, res.data[0].lon);
         console.log(reW);
+        let reM = await getMoviesData(searchQuery);
+        console.log(reM);
+
       }
     var  getWeatherData = async (lat,lon) =>{
-        const API = `http://localhost:3001/weather?lat=${lat}&lon=${lon}&searchQuery=${searchQuery}`;
+        const API = `http://localhost:3001/weather?lat=${lat}&lon=${lon}`;
         console.log(API);
         try {
-                    const res = await axios.get(API);
+        const res = await axios.get(API);
         console.log(res);
         setWeatherData(res.data)
         return res;
-
         } catch (error) {
             console.error(error);
         }
     }
+
+    var  getMoviesData = async (searchQuery) =>{
+        const API = `http://localhost:3001/movies?searchQuery=${searchQuery}`;
+        console.log(API);
+        try {
+        const res = await axios.get(API);
+        console.log(res);
+        setMoviesData(res.data)
+        return res;
+        } catch (error) {
+            console.error(error);
+        }
+        
+    }
+
     return (
         <div>
             <Form>
@@ -44,6 +63,7 @@ function CityForm() {
             {location.place_id && <h2>The city is: {location.display_name} ,{location.lat}, {location.lon}</h2>}
             <CityMap location={location}/>
             <Weather weatherData={weatherData}/>
+            <Movies moviesData={moviesData}/>
 
         </div>
     );
